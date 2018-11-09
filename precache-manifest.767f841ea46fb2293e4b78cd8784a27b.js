70df0a4d88f97807abc0eee241abfc48519e1530
Hiren Tariwala <hirentariwala19@gmail.com>
2018-11-09 19:27:48 +0530

    Updates

---
+++ precache-manifest.767f841ea46fb2293e4b78cd8784a27b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f01411798c35bf04416ecc7545b705f",
+    "url": "/static/media/unionschool.6f014117.jpg"
+  },
+  {
+    "revision": "1bc11837a25c0068b678ddcd8d4df812",
+    "url": "/static/media/sass.1bc11837.png"
+  },
+  {
+    "revision": "2b4a5e4e4991152c68430f95199450a1",
+    "url": "/static/media/hiren.2b4a5e4e.jpg"
+  },
+  {
+    "revision": "ed0bd7af38cf2ed8864590452deeb8db",
+    "url": "/static/media/bmiit.ed0bd7af.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "033282c104595cf6dbe8",
+    "url": "/static/js/main.033282c1.chunk.js"
+  },
+  {
+    "revision": "43353c1e4d69d57d6e2e",
+    "url": "/static/js/1.43353c1e.chunk.js"
+  },
+  {
+    "revision": "033282c104595cf6dbe8",
+    "url": "/static/css/main.736b698a.chunk.css"
+  },
+  {
+    "revision": "ef81a7510a64177483cddaa93a87d2d8",
+    "url": "/index.html"
+  }
+];
